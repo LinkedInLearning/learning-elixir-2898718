fbebb53f929c076084d0a416e28381ab27f4571e
teach <teach@vanessas-mbp-2.attlocal.net>
2021-11-13 10:48:20 -0600

    finalize files

--- wizard/lib/chap_02/boolean.ex
+++ wizard/lib/chap_02/boolean.ex
@@ -1,31 +1,14 @@
-defmodule Wizard.Chap02.Boolean do
+defmodule Wizard.Boolean do
   @moduledoc """
-  This module contains functions for learning about the Boolean data type in Elixir.
-
-  # Variables
-  Variable names can include uppercase or lowercase letters, underscores, or numbers,
-  but they must start with a lowercase letter or an underscore.
-
-  # Immutability
-  Variables are immutable. Once a variable is bound, it cannot change. It can only be rebound.
+  This module contains functions for learning about
+  the Boolean data type.
   """
 
   @doc """
-  Define a boolean
+  The Kernel function `is_boolean` will return TRUE if
+  the argument it receives is a boolean.
   """
-  def define() do
-    definition = "A Boolean is either the atom true or the atom false. The atom nil evaluates to false in control flow."
-    {:ok, definition}
-  end
-
-  @doc """
-  The Kernel function `is_boolean` will return true if the argument it receives
-  is a boolean.
-  """
-  def booleans() do
-    IO.puts "true? #{is_boolean(true)}"
-    IO.puts "\"true\"? #{is_boolean("true")}"
-    IO.puts "false? #{is_boolean(false)}"
-    {:ok, "Booleans!"}
+  def check(b) do
+    is_boolean(b)
   end
 end
--- wizard/lib/chap_02/integer.ex
+++ wizard/lib/chap_02/integer.ex
@@ -1,66 +1,36 @@
-defmodule Wizard.Chap02.Integer do
+defmodule Wizard.Integer do
   @moduledoc """
-  This module contains functions for learning about the Integer and Float data types in Elixir.
-
-  # Variables
-  Variable names can include uppercase or lowercase letters, underscores, or numbers,
-  but they must start with a lowercase letter or an underscore.
-
-  # Immutability
-  Variables are immutable. Once a variable is bound, it cannot change. It can only be rebound.
+  This module contains functions for learning about
+  the Integer and Float data types.
   """
 
   @doc """
-  The Kernel function `is_integer` will return true if the argument it receives
-  is an integer
+  The Kernel function `is_integer` returns TRUE if
+  the argument it receives is an integer.
 
-  Large numbers are separated by the underscore to improve readability. For example,
-  1000000 becomes 1_000_000
+  The Kernel function `is_float` returns TRUE if the
+  argument it receives is a float.
   """
-  def integers do
-    int_1 = 1
-    int_2 = 234
-    int_3 = 567_890
-
-    IO.puts "The variable int_1 is bound #{int_1}."
-    IO.puts "Truly an integer? #{is_integer(int_1)}"
-
-    IO.puts "The variable int_2 is bound #{int_2}."
-    IO.puts "Truly an integer? #{is_integer(int_2)}"
-
-    IO.puts "The variable int_3 is bound #{int_3}."
-    IO.puts "Truly an integer? #{is_integer(int_3)}"
-
-    {:ok, "Integers!"}
+  def check(value) do
+    IO.puts "Integer? #{is_integer(value)}"
+    IO.puts "Float? #{is_float(value)}"
   end
 
   @doc """
-  The Kernel function `is_float` will return true if the argument it receives
-  is a float.
-
-  Floats are a sequence of digits that contain a dot and at least one other digit after the dot.
-  Floats can also be written in scientific notation.
+  Demo of Integer and Float modules.
   """
-  def floats do
-    float1 = 1.23
-    float2 = 123.4e5
+  def demo do
+    i = 245
+    f = 3.14159
+    s = to_string(i)
 
-    IO.puts "Float #1 is #{float1}."
-    IO.puts "Truly a float? #{is_float(float1)}"
+    IO.puts "Integer: #{i}"
+    IO.inspect(Integer.parse(s), label: "Parse")
+    IO.inspect(Integer.digits(i), label: "Digits")
 
-    IO.puts "Float #2 is #{float2}."
-    IO.puts "Truly a float? #{is_float(float2)}"
-    {:ok, "Floats!"}
-  end
-
-  @doc """
-  The Kernel function `is_boolean` will return true if the argument it receives
-  is a boolean.
-  """
-  def booleans() do
-    IO.puts "true? #{is_boolean(true)}"
-    IO.puts "\"true\"? #{is_boolean("true")}"
-    IO.puts "false? #{is_boolean(false)}"
-    {:ok, "Booleans!"}
+    IO.puts "Float: #{f}"
+    IO.puts "Round: #{Float.round(f)}"
+    IO.puts "Round to 1: #{Float.round(f, 1)}"
+    IO.inspect(Float.parse(s), label: "Parse")
   end
 end
