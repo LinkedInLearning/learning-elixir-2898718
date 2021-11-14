49f787fea487caf88633f09868b0aa73e41a356b
teach <teach@vanessas-mbp-2.attlocal.net>
2021-11-13 20:03:24 -0600

    solution

--- wizard/lib/chap_03/logic_exercise.ex
+++ wizard/lib/chap_03/logic_exercise.ex
@@ -18,38 +18,37 @@ defmodule Wizard.LogicExercise do
   """
 
   def run() do
-    answer_1 = nil
+    answer_1 = 3 
     test_1 = is_integer(answer_1)
 
-    # replace the answer variable value with a value that will make the test variable equal true
-    answer_2 = nil
+    answer_2 = 3.25 
     test_2 = is_float(answer_2)
 
-    answer_3 = nil
+    answer_3 = "string" 
     test_3 = is_binary(answer_3)
 
-    answer_4 = nil
+    answer_4 = [3, 10, "string"] 
     test_4 = is_list(answer_4)
 
-    answer_5 = nil
+    answer_5 = 1 # or any value that evaluates to true
     test_5 = true and answer_5
 
-    answer_6 = nil
+    answer_6 = "string" # or any value that evalutes to true
     test_6 = nil || answer_6
 
-    answer_7 = nil
-    test_7 = !answer_7
+    answer_7 = false # or any value that evalutes to false
+    test_7 = not answer_7
 
-    answer_8 = nil
+    answer_8 = "not an atom" 
     test_8 = !is_atom(answer_8)
 
-    answer_9 = nil
+    answer_9 = :string # or any value that evalutes to true
     test_9 = false or answer_9
 
-    answer_10 = ""
+    answer_10 = "string" 
     test_10 = String.length(answer_10) && true
 
-    answer_11 = %{}
+    answer_11 = %{"distance" => "20km"} 
     test_11 = Map.get(answer_11, "distance") && true
 
     test_1 && test_2 && test_3 && test_4 && test_5 && test_6 && test_7 && test_8 && test_9 && test_10 && test_11
