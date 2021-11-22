67a7c31003333b510407844da2b9df410a3e9575
teach <teach@vanessas-mbp-2.attlocal.net>
2021-11-21 20:05:56 -0600

    ending

--- wizard/lib/chap_04/pin.ex
+++ wizard/lib/chap_04/pin.ex
@@ -13,10 +13,15 @@ defmodule Wizard.Pin do
     %{spell: "luck", type: "good"}
     iex> Wizard.Pin.spell(:type, potion)
     "This spell is good luck"
+    iex> Wizard.Pin.spell(:type, %{spell: "luck"})
+    ** (MatchError) no match of right hand side value: %{spell: "luck"}
+    lib/chap_04/pin.ex:18: Wizard.Pin.spell/2
+    iex> Wizard.Pin.spell(:spell, %{spell: "luck"})
+    %{spell: "luck"}
   """
   def spell(type, map) do
-    # %{^type => a} = map
-    # "This spell is #{a}."
+    %{^type => a} = map
+    "This spell is #{a}."
   end
 
 end
