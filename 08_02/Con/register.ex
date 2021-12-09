1dc8b093d6b07f9e52254354159e7b7010c5be5c
Vanessa Lee <vanessaklee@gmail.com>
2021-12-08 23:32:51 -0600

    Create register.ex

---
+++ 08_02/Con/register.ex
@@ -0,0 +1,15 @@
+defmodule Wizard.Con.Register do
+  @moduledoc """
+  Registration functions for the wizard convention.
+  """
+  alias Wizard.Con.Guest
+  alias Wizard.Con.EventSchedule, as: Schedule
+
+  @doc """
+  Create a new WizCon registration. Take a name, create a guest, and return a schedule.
+  """
+  def new(name \\ "Anonymous") do
+    Guest.new(name)
+    |> Schedule.display()
+  end
+end
