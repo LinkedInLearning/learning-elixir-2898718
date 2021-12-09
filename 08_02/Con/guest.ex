e5c4d1471e2e634fba9e3375a4c35c811a1a552f
Vanessa Lee <vanessaklee@gmail.com>
2021-12-09 00:49:32 -0600

    Update guest.ex

--- 08_02/Con/guest.ex
+++ 08_02/Con/guest.ex
@@ -2,18 +2,18 @@ defmodule Wizard.Con.Guest do
   @moduledoc """
   Function to handle event guests.
   """
-  @specialties ["Elements", "Incantations", "Abjuration", "Enchantment"]
+  @default_name "No Name"
   alias Wizard.Con.EventSchedule, as: Schedule
   
   @doc """
   Accept a name and return a guest map with the name, a randomly generated ID, list of events, and a specialty.
   """
+  def new(nil), do: new(@default_name)
+  
   def new(name) do
     id = :rand.uniform(100)
-    speciality = Enum.random(@specialties)
-    events = Schedule.events()
 
-    %{id: id, name: name, speciality: speciality}
+    %{id: id, name: name}
   end
   
   // def new(name, speciality) do
