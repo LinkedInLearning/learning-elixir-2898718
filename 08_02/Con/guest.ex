a5373a8691b0ddf26c1d03800f0739acc0a0785b
Vanessa Lee <vanessaklee@gmail.com>
2021-12-08 23:33:29 -0600

    Update guest.ex

--- 08_02/Con/guest.ex
+++ 08_02/Con/guest.ex
@@ -2,11 +2,13 @@ defmodule Wizard.Con.Guest do
   @moduledoc """
   Handle event guests.
   """
+  @specialties ["Elements", "Incantations", "Abjuration", "Enchantment"]
+  alias Wizard.Con.EventSchedule, as: Schedule
   
   defp new(name) do
     id = :rand.uniform(100)
     speciality = Enum.random(@specialties)
-    events = events()
+    events = Schedule.events()
 
     %{id: id, name: name, events: events, speciality: speciality}
   end
