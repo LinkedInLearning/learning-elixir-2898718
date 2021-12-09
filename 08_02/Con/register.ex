ff184efd4981d658918a1c8f1c72d723dea26bc9
Vanessa Lee <vanessaklee@gmail.com>
2021-12-09 00:03:53 -0600

    Update register.ex

--- 08_02/Con/register.ex
+++ 08_02/Con/register.ex
@@ -8,8 +8,13 @@ defmodule Wizard.Con.Register do
   @doc """
   Create a new WizCon registration. Take a name, create a guest, and return a schedule.
   """
-  def new(name \\ "Anonymous") do
+  def new(name) do
     Guest.new(name)
     |> Schedule.display()
   end
+  
+  // def new(name, speciality \\ "Curiosity") do
+    // Guest.new(name, speciality)
+    // |> Schedule.display()
+  // end
 end
