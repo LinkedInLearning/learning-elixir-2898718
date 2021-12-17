61abb91d9e09e969ae4306ebe65c074834dc4e55
teach <teach@vanessas-mbp-2.attlocal.net>
2021-12-17 10:05:04 -0600

    init

--- wizard/test/mixture_test.exs
+++ wizard/test/mixture_test.exs
@@ -1,5 +1,11 @@
 defmodule Wizard.MixtureTest do
-  
+  use ExUnit.Case
 
+  test "First mixture will be ready before second mixture" do
+    first = Wizard.Mixture.mix_time()
+    second = Wizard.Mixture.mix_time()
+
+    assert Timex.before?(first, second)
+  end
   
 end
